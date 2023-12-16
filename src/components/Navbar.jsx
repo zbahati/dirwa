504f880d2fd694ac99cdc17fb374f1a7269fd681
zbahati <bahatizirimwabagabo@gmail.com>
2023-12-16 22:00:54 +0200

    Add navbar with the responsiveness for all devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,110 +1,191 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {FaBars} from 'react-icons/fa6';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa6';
+import { FaX } from "react-icons/fa6";
 import logo from '../assets/logo.jpg';
 
 const Navbar = () => {
-  return (
-    <header className='p-2 mx-auto'>
-      <div className="flex justify-between items-center">
-        <div className='flex gap-2 items-center'>
-          <img src={logo} alt="logo" className="h-28 w-28"/>
-          <h1 className='text-2xl md:text-3xl lg:text-4xl'>Duhumurizanye Iwacu Rwanda</h1>
-        </div>
-        <button type="button">Donate</button>
-        <button type="button">
-          <FaBars size={30}/>
-        </button>
-      </div>
-      <nav>
-        <ul className="">
-          <li>
-            <Link to="">About us</Link>
-          </li>
-          <li>
-            <Link to="">
-              <span>Participants</span>
-              <ul>
-                <li>
-                  <Link to="">Youth</Link>
-                </li>
-                <li>
-                  <Link to="">Women</Link>
-                </li>
-                <li>
-                  <Link to="">Conflict families
-                  </Link>
-                </li>
-                <li>
-                  <Link to="">Refugees
-                  </Link>
-                </li>
-                <li>
-                  <Link to="">Community leaders
-                  </Link>
-                </li>
-              </ul>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <span>Intervetion</span>
-              <ul>
-                <li>
-                  <Link to="/Healing">Healing and Peace Building</Link>
-                </li>
-                <li>
-                  <Link to="/Psycho_social_care">Psycho-social Care</Link>
-                </li>
-                <li>
-                  <Link to="/Asset_base_community">Asset Base Community Development</Link>
-                </li>
-                <li>
-                  <Link to="/Active_by_standship">Active by Standship Development</Link>
-                </li>
-                <li>
-                  <Link to="/how_we_help">How we help</Link>
-                </li>
-              </ul>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Where_we_work">where we work</Link>
-          </li>
-          <li>
-            <Link to="">
-              <span>Publications</span>
-              <ul>
-                <li>
-                  <Link to="">News</Link>
-                </li>
-                <li>
-                  <Link to="">Stories</Link>
-                </li>
-                <li>
-                  <Link to="">Announcements
-                  </Link>
-                </li>
-              </ul>
-            </Link>
-          </li>
-          <li>
-            <Link to="">
-              <span>Join us</span>
-              <ul>
-                <li>
-                  <Link to="">Volunteer</Link>
-                </li>
-                <li>
-                  <Link to="">Internship Program</Link>
-                </li>
-              </ul>
-            </Link>
-          </li>
+  const [clickIntervention, setIntervention] = useState(false);
+  const [clickPublication, setPublication] = useState(false);
+  const [clickJoinUs, setJoinUs] = useState(false);
+  const [clickParticipation, setParticipation] = useState(false);
+  const [clickAboutUs, setAboutUs] = useState(false);
+  const [clickWhereWeWork, setWhereWeWork] = useState(false);
+  const [nav, setNav] = useState(false);
 
-        </ul>
-      </nav>
-    </header>
+  const handleIntervention = () => {
+    setIntervention(!clickIntervention);
+    setJoinUs(false);
+    setParticipation(false);
+    setPublication(false);
+    setAboutUs(false);
+    setWhereWeWork(false);
+  };
+
+  const handlePublication = () => {
+    setPublication(!clickPublication);
+    setJoinUs(false);
+    setIntervention(false);
+    setParticipation(false);
+    setAboutUs(false);
+    setWhereWeWork(false);
+  };
+
+  const handleJoinUs = () => {
+    setJoinUs(!clickJoinUs);
+    setIntervention(false);
+    setParticipation(false);
+    setPublication(false);
+    setAboutUs(false);
+    setWhereWeWork(false);
+  };
+
+  const handleParticipation = () => {
+    setParticipation(!clickParticipation);
+    setJoinUs(false);
+    setIntervention(false);
+    setPublication(false);
+    setAboutUs(false);
+    setWhereWeWork(false);
+  };
+
+  const handleWhereWeWork = () => {
+    setWhereWeWork(!clickWhereWeWork);
+    setJoinUs(false);
+    setIntervention(false);
+    setParticipation(false);
+    setPublication(false);
+    setAboutUs(false);
+    setNav(false)
+  };
+
+  const handleAboutUs = () => {
+    setAboutUs(!clickAboutUs);
+    setJoinUs(false);
+    setIntervention(false);
+    setParticipation(false);
+    setPublication(false);
+    setWhereWeWork(false);
+    setNav(false)
+  };
+
+  const handleToggleMenu = () => {
+    setNav(!nav);
+  };
+
+  const handleHideSidebar = () => {
+    setAboutUs(false);
+    setPublication(false);
+    setJoinUs(false);
+    setParticipation(false);
+    setNav(false)
+  }
+
+  return (
+    <div>
+      <header className='p-2 mx-auto'>
+        <div className="flex justify-between items-center">
+          <div className='flex gap-2 items-center'>
+            <img src={logo} alt="logo" className="h-28 w-28" />
+            <h1 className='hidden md:text-2xl md:inline lg:text-3xl capitalize'>Duhumurizanye Iwacu Rwanda</h1>
+          </div>
+          <button
+            type="button"
+            className='hidden sm:inline w-36 h-12 bg-blue-400 text-white text-2xl outline-none rounded-lg hover:bg-blue-300 cursor-pointer'>Donate</button>
+          {nav ? <button type='button' onClick={handleHideSidebar} className='absolute text-2xl text-white cursor-pointer z-30 top-0 right-2 pt-3'><FaX /></button> : <button type="button" onClick={handleToggleMenu} className='sm:hidden'>
+            <FaBars size={30} />
+          </button>
+          }
+        </div>
+        <nav>
+          <ul className={nav ? 'flex flex-col text-xl gap-9 h-screen absolute w-full duration-300 top-0 bottom-0 left-0 bg-blue-400 pt-3 z-20 px-2 rounded-md' : 'hidden sm:flex sm:justify-between sm:items-center sm:border-2 sm:p-2'}>
+            <li className='cursor-pointer' onClick={handleAboutUs}>
+              <Link to="" className={clickAboutUs ? 'underline underline-offset-8' : ''}>About us</Link>
+            </li>
+            <li onClick={handleParticipation} className={`relative cursor-pointer ${clickParticipation ? 'underline underline-offset-8' : ''}`}>
+              <span>Participants</span>
+              <ul className={clickParticipation ? ' bg-slate-300 w-64 absolute py-2 my-4 z-10 underline underline-offset-8 border-2' : 'hidden'}>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='mx-3'>Youth</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2' >
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Women</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Conflict families
+                  </Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Refugees
+                  </Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Community leaders
+                  </Link>
+                </li>
+              </ul>
+
+            </li>
+            <li className={`relative cursor-pointer ${clickIntervention ? 'underline underline-offset-8' : ''}`} onClick={handleIntervention}>
+              <span>Intervetion</span>
+              <ul className={clickIntervention ? ' bg-slate-300 w-[340px] absolute py-2 my-4 z-10 underline underline-offset-8 border-2' : 'hidden'} >
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="/Healing" onClick={handleHideSidebar} className='my-4 mx-3'>Healing and Peace Building</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="/Psycho_social_care" onClick={handleHideSidebar} className='my-4 mx-3'>Psycho-social Care</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="/Asset_base_community" onClick={handleHideSidebar} className='my-4 mx-3'>Asset Base Community Development</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="/Active_by_standship" onClick={handleHideSidebar} className='my-4 mx-3'>Active by Standship Development</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="/how_we_help" onClick={handleHideSidebar} className='my-4 mx-3'>How we help</Link>
+                </li>
+              </ul>
+            </li>
+            <li onClick={handleWhereWeWork} className='cursor-pointer'>
+              <Link to="/Where_we_work" onClick={handleHideSidebar} className={clickWhereWeWork ? 'underline underline-offset-8' : ''} >where we work</Link>
+            </li>
+            <li className={`cursor-pointer ${clickPublication ? 'underline underline-offset-8' : ''}`} onClick={handlePublication}>
+              <span>Publications</span>
+              <ul className={clickPublication ? ' bg-slate-300 w-64 absolute py-2 my-4 z-10 underline underline-offset-8 border-2' : 'hidden'} >
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>News</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Stories</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar} className='my-4 mx-3'>Announcements
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className={`relative cursor-pointer ${clickJoinUs ? 'underline underline-offset-8' : ''}`} onClick={handleJoinUs}>
+              <span>Join us</span>
+              <ul className={clickJoinUs && nav ? ' bg-slate-300 absolute w-64 py-2 my-4 z-10 underline underline-offset-8 border-2' : clickJoinUs ? ' bg-slate-300 absolute w-64 transform translate-x-[-200px] py-2 my-4 z-10 underline underline-offset-8 border-2' : 'hidden'} >
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar}>Volunteer</Link>
+                </li>
+                <li className='hover:bg-slate-50 py-2'>
+                  <Link to="" onClick={handleHideSidebar}>Internship Program</Link>
+                </li>
+              </ul>
+            </li>
+
+          </ul>
+        </nav>
+      </header>
+      {
+        nav ? <div className='w-full h-screen left-0 top-0 bottom-0 absolute bg-gray-300 /50'></div>
+          : ''
+      }
+
+    </div >
+
   );
 };
 
